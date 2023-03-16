0d5358175aca6e527aabffdf50b15134e7836a28
Ajaykj684 <Ajaykj684@gmail.com>
2023-03-16 11:10:13 +0530

    last update

--- DreamProject/settings.py
+++ DreamProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h-mzu+0z*(b9tqoyqfcla+rq$rq)7^(h&888&m*-0&fxymq@m8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
